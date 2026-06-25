5e0ae84b4f6b94d79119e9bbebc9b21f8dd40cc8
Salah <ahmed.msalah99@gmail.com>
2026-06-25 17:53:15 +0200

    changing tag pose and ned

--- traj_gen_test/launch/dummy_test.launch.py
+++ traj_gen_test/launch/dummy_test.launch.py
@@ -20,8 +20,8 @@ def generate_launch_description():
                 'waypoints': [1.0, 0.0, -1.0, 0.0,
                               2.0, 1.0, -1.0, 0.0,
                               3.0, 0.0, -1.0, 0.0],
-                'tag_pose' : [3.0, 0.0, 1.0, 0.0,3.14,0.0],
-                'odom_ned':[-5.0,0,0],
+                'tag_pose' : [2.0, 0.0, 0.0, 0.0,3.14,0.0],
+                'odom_ned':[1.0, 0.0, -1.0,0],
                 'publish_tf': True,      # publish TF so RViz can resolve the frames
                 'fixed_frame': 'map',    # set RViz Fixed Frame to this
                 'rviz_enu_flip': True,   # flip fixed_frame->planner so NED shows upright
