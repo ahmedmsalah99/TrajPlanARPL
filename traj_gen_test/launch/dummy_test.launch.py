38ab55f4cb7734b2407506d35e2ac03feafb9322
Salah <ahmed.msalah99@gmail.com>
2026-07-06 20:00:59 +0200

    adjusting a simpler test case

--- traj_gen_test/launch/dummy_test.launch.py
+++ traj_gen_test/launch/dummy_test.launch.py
@@ -20,8 +20,8 @@ def generate_launch_description():
                 'waypoints': [
                     # 0.0, 0.0, 0.0, 0.0,
                             #   2.0, 0.0, -1.0, 0.0,
-                              3.0, 0.0, -1.0, 0.0],
-                'tag_pose' : [10, 0, 5.0, 3.54, 0.3, 0],
+                              3.0, 0.0, 0.0, 0.0],
+                'tag_pose' : [10.0, 0.0, 0.0, 3.54, 0.3, 0],
                 'odom_ned':[0.0, 0.0, -5,0],
                 'publish_tf': True,      # publish TF so RViz can resolve the frames
                 'fixed_frame': 'map',    # set RViz Fixed Frame to this
